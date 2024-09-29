e0e24bf0e2e7c5f33e68206a02289e6c5a921537
MadhuSRM <157085016+MadhuSRM@users.noreply.github.com>
2024-09-29 19:10:45 +0530

    Create controlStructures.py

---
+++ Programs/controlStructures.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 21 13:43:49 2024
+
+@author: hp
+"""
+
+#Reverse Number - while demo
+def reverseNumber(num):
+    result = temp = 0
+    while(num > 0):
+        temp = num % 10
+        result = result*10 + temp
+        num = num//10 #integer division
+    return result
+
+#Reverse Words - for demo
+def reverseWords(str):
+    result = ""
+    strArry = str.split();
+    for i in range(len(strArry)-1,-1,-1):
+        result = result + " "+strArry[i]
+    return result
+
+#Recursive Function - Fibonacci 
+#if - elif - else demo
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+    
+num1 = int(input("Enter the number to be reversed:"))
+print(reverseNumber(num1))
+
+str = input("Enter the words to be reversed:")
+print(reverseWords(str))
+
+num2 = int(input("Enter the number:"))
+print(fib(num2))
